28fe1b86ade8bab1e38a0830c81c9d25531358a8
nicollas0eu <92069748+nicollas0eu@users.noreply.github.com>
2021-11-18 23:04:13 -0300

    Add files via upload

---
+++ quadrado.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO2Ex2
+{
+    class AreaQuadrado
+    {
+        private double aresta;
+        private double resultado;
+
+        #region constutores
+
+        public AreaQuadrado()
+        {
+            this.aresta = 0;
+        }
+
+        public AreaQuadrado(double aresta, double resultado)
+        {
+            this.aresta = aresta;
+        }
+
+        #endregion
+
+        #region getset
+
+        public void setAresta(double x)
+        {
+            this.aresta = x;
+        }
+
+        public double getAresta()
+        {
+            return this.aresta;
+        }
+
+        public double getResultado()
+        {
+            return this.resultado;
+        }
+
+        #endregion
+
+        public void calcular()
+        {
+            this.resultado = Math.Pow(this.aresta, 2);
+        }
+        
+    }
+}
